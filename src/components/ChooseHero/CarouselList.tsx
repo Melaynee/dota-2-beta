4c29a1e8d380a29274813d59c1c7403c2b8794ca
Melaynee <melayneeyt@gmail.com>
2024-01-30 17:36:22 +0100

    added data slices in carousel

--- src/components/ChooseHero/CarouselList.tsx
+++ src/components/ChooseHero/CarouselList.tsx
@@ -26,11 +26,11 @@ const CarouselList = () => {
   return (
     <div className="relative">
       <div className="flex flex-col w-[200px] sm:w-[400px] md:w-[600px] lg:w-[800px] xl:w-[1200px] mx-auto">
-        <Carousel direction="left" speed={50} data={heroes} />
-        <Carousel direction="right" speed={40} data={heroes} />
-        <Carousel direction="left" speed={60} data={heroes} />
-        <Carousel direction="right" speed={20} data={heroes} />
-        <Carousel direction="left" speed={40} data={heroes} />
+        <Carousel direction="left" speed={50} data={heroes.slice(0, 34)} />
+        <Carousel direction="right" speed={40} data={heroes.slice(34, 68)} />
+        <Carousel direction="left" speed={60} data={heroes.slice(68, 102)} />
+        <Carousel direction="right" speed={20} data={heroes.slice(102)} />
+        <Carousel direction="left" speed={40} data={heroes.slice(20, 54)} />
         <div className="bg-gradient-to-r from-black via-black to-transparent absolute top-0 left-0 w-1/3 h-full z-10"></div>
         <div className="bg-gradient-to-l from-black via-black to-transparent absolute top-0 right-0 w-1/3 h-full z-10"></div>
       </div>
