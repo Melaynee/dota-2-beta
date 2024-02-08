fbe02c58c37e779a3fc8234b60bda82d5ed81118
Melaynee <melayneeyt@gmail.com>
2024-02-08 12:26:30 +0100

    added queries

--- src/components/Esports/Options.tsx
+++ src/components/Esports/Options.tsx
@@ -3,9 +3,11 @@ import Link from "next/link";
 import React from "react";
 import { useCurrentPathName } from "../useRouterPathname";
 import SelectLeagueButton from "../SelectLeagueButton";
+import { useSearchParams } from "next/navigation";
 
 const Options = ({ onClick }: { onClick: () => void }) => {
-  const pathname = useCurrentPathName();
+  const params = useSearchParams();
+  const active = params.get("tab") ?? "esports";
   return (
     <div className="bg-[#131315] w-full sticky top-20 z-20 shadow-[0_10px_20px_0_rgba(0,0,0,0.5)] flex justify-center">
       <div className="flex -ml-28 mr-20 items-center">
@@ -13,9 +15,10 @@ const Options = ({ onClick }: { onClick: () => void }) => {
       </div>
       <div className="flex justify-between w-[350px] uppercase text-[#6B7785] font-semibold tracking-[0.2rem] text-sm pt-1">
         <Link
-          href="/watch"
+          scroll={false}
+          href="/esports"
           className={`hover:text-white py-4 px-5 ${
-            pathname === "esports"
+            active === "esports"
               ? "text-white border-b-[3px] border-[#FF6046]"
               : ""
           }`}
@@ -23,9 +26,10 @@ const Options = ({ onClick }: { onClick: () => void }) => {
           Watch
         </Link>
         <Link
-          href="/esports/schedule"
+          scroll={false}
+          href={{ query: { tab: "schedule" } }}
           className={`hover:text-white py-4 px-5 ${
-            pathname === "schedule"
+            active === "schedule"
               ? "text-white border-b-[3px] border-[#FF6046]"
               : ""
           }`}
@@ -33,9 +37,10 @@ const Options = ({ onClick }: { onClick: () => void }) => {
           Schedule
         </Link>
         <Link
-          href="/esports/standings"
+          scroll={false}
+          href={{ query: { tab: "standings" } }}
           className={`hover:text-white py-4 px-5 ${
-            pathname === "standings"
+            active === "standings"
               ? "text-white border-b-[3px] border-[#FF6046]"
               : ""
           }`}
